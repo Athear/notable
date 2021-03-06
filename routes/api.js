42acc28182b5dc19c1b1f37d02e90228f128eeb9
Todd Malone <toddomalone@gmail.com>
2021-03-05 21:21:14 -0600

    added functionality for GET method on notes

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,12 @@
+const fs = require("fs");
+
 module.exports = function (app) {
     app.get("/api/notes", function(req,res){
-
+      fs.readFile(`${__dirname}/../db/db.json`, (err, data) => {
+        if (err) throw err;
+        allNotes = JSON.parse(data)
+        res.json(allNotes);
+      });
     })
 
 
