f4c8a686cfb3a61cc2467b8acb99b2c98b637c7a
Todd Malone <toddomalone@gmail.com>
2021-03-05 21:03:11 -0600

    set up routes to serve HTML

--- routes/html.js
+++ routes/html.js
@@ -1,11 +1,20 @@
+const fs = require("fs");
 module.exports = function (app) {
     app.get("/notes", function(req,res){
-
+      fs.readFile(`${__dirname}/../public/notes.html`, (err,data)=>{
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      })
     })
 
 
     app.get("/*", function(req,res){
-
+      fs.readFile(`${__dirname}/../public/index.html`, (err,data)=>{
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      })
     })
 
   };
